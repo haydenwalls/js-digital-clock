b58efa2b844786ffcb8b5fe700a36c8f6d234fa7
Hayden Walls <hwilliams.deva@gmail.com>
2018-11-17 20:17:28 -0800

    implement seizure clock

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+$(document).ready(function() {
+  const colors = ['#45FF92', '#FFCC35', '#FF87C5', '#45AEFF', '#FFFF32', '#FF4F38', '#4FFFF3', '#4F4FFF', '#FF87FB']
+  let time = new Date();
+  const randomColor = function(colors) {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+  };
+
+  $('div#upper-left').html(time.getHours() % 12 || 12);
+  // $('div#upper-left').css('color', randomColor(colors));
+  $('div#upper-right').html(time.getMinutes());
+  // $('div#upper-right').css('color', randomColor(colors));
+  $('div#lower-right').html(time.getSeconds());
+  // $('div#lower-right').css('color', randomColor(colors));
+  $('div#lower-left').html(time.getMilliseconds());
+  // $('div#lower-left').css('color', randomColor(colors));
+
+  setInterval(() => {
+    time = new Date;
+
+    $('div#lower-left').html(time.getMilliseconds());
+    $('body').css('backgroundColor', randomColor(colors));
+  }, 63);
+
+  setInterval(function() {
+    $('div#upper-left').html(time.getHours() % 12 || 12);
+    $('div#upper-right').html(time.getMinutes());
+    $('div#lower-right').html(time.getSeconds());
+  }, 1000);
+
+  let i = 0
+  setInterval(function() {
+    if (i % 10 == 0) {
+      $('div#center').append('<span class="dot">\n.</span>');
+      i++;
+    } else if (i < 59) {
+      $('div#center').append('<span class="dot">.</span>');
+      i++;
+    } else {
+      i = 0;
+      $('div#center').html('');
+    }
+  }, 1000);
+});
